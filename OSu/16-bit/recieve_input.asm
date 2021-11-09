af95e0f4dd5e8b4f901016b0d6168ff45f7395ca
Zman2024 <55037860+Zman2024@users.noreply.github.com>
2021-11-09 11:50:46 -0700

    Fix issue #2 "No backspacing in 16 bit real mode"
    Fixed by printing a space, then a backspace after the user inputs a backspace

--- OSu/16-bit/recieve_input.asm
+++ OSu/16-bit/recieve_input.asm
@@ -10,51 +10,18 @@ loop_input:
 	mov ah, 0x00
 	cmp bl, 0x0d				; If the Scan code turns out to be 0x0d (return carriage) then jump to the end
 	je print_input_loop
+	cmp bl, 0x08				; compare to \b (Backspace)
+	je backspace				; run backspace subroutine
+
 	int 0x16					; Read keyboard input
 
 	mov bx, ax					; mov ax to bx
 	and bx, 0x00ff				; get the scan codes character
-
-	cmp bl, 0x08
-	je remove_previous_character
-
 	push bx						; push bx onto the stack
 
 	mov ah, 0x0e				; Enter TTY mode
 	mov al, bl					; print the value stored in bl to the BIOS
 	int 0x10					; Call BIOS interrupt
-
-
-	jmp loop_input
-remove_previous_character:
-	mov ah, 0x03				; get the curesor position on the console
-	mov bh, 0 					; Set the page to 0
-	int 0x10					; DH -> Row, DL -> Column
-
-
-	;call print_new_line
-	;call print_hex
-	cmp dl, 0
-	je loop_input
-
-
-	cmp bp, sp
-	je loop_input
-
-	pop bx
-	sub dl, 1
-
-	mov ah, 0x02
-	mov bh, 1
-	;call print_new_line
-	;call print_hex
-	int 0x10
-
-	mov ah, 0x0a
-	mov al, 0
-	mov bh, 0
-	mov cx, 1
-	int 0x10
 
 	jmp loop_input
 
@@ -81,3 +48,12 @@ end_input_loop:					; end of the function
 
 	popa						; restore the previous stack
 	ret							; return
+
+backspace:
+	mov ah, 0x0e				; Enter TTY mode
+	mov al, " "					; print space character to erase old char
+	int 0x10					; Call BIOS interrupt
+	mov al, 0x08				; move backspace char back into al
+	int 0x10					; Call BIOS interrupt to move cursor back again
+	xor bl, bl					; zero bl
+jmp loop_input
