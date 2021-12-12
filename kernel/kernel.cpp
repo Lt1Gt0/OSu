241f27e90f934aa7c4827774657767757f2cfbd9
SKaminski <47929278+Lt1Gt0@users.noreply.github.com>
2021-12-12 00:35:44 -0700

    Delete kernel directory

--- kernel/kernel.cpp
+++
@@ -1,7 +0,0 @@
-#include "PrintText.cpp"
-
-extern "C" void _start(){
-	SetCursorPosition(CursorPositionCoords(0,0)); //Set the cursor to the top left by default
-	PrintString("Dice Roll Game\nBruh\nMy Cock burns alot");
-	return;
-}
