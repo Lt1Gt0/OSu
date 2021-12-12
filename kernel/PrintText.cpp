97dc1adaf5cca47e24e98f46416e94118c863e85
Simon <simonkaminski1100@gmail.com>
2021-12-11 23:13:54 -0700

    Added typedefs

--- kernel/PrintText.cpp
+++ kernel/PrintText.cpp
@@ -1,30 +1,34 @@
 #pragma once
 #include "IO.cpp"
-#define VGA_MEMORY (unsigned char*)0xb8000
-#define VGA_WIDTH 160 
-#define VGA_HEIGHT 50
+#include "Typedefs.cpp"
+#define VGA_MEMORY (uint_8*)0xb8000
+#define VGA_WIDTH 80 
+#define VGA_HEIGHT 25
 
 
-unsigned short cursorPos;
+uint_8 cursorPos;
 
-//TODO Update cursor position based off of a scale of rows and columns
-unsigned short scale_cursor_position(unsigned short x, unsigned short y){
+uint_8 scale_cursor_position(uint_8 x, uint_8 y){
 	unsigned short row;
 	unsigned short col;
-	if(y > VGA_HEIGHT){
-		// OVERFLOW OF ROWS
-	}
 
+	// Might need ??
+	// if(y >= VGA_HEIGHT)
+	// 	y = VGA_HEIGHT - 1; //Set the Cursor y to the maximum it could be
+	// if(x >= VGA_WIDTH)
+	// 	x = VGA_WIDTH - 1; //Set the Cursor x to the maximum it could be
 
-	return (y * (VGA_WIDTH/2)) + (x/2);
+	return (y * VGA_WIDTH) + x;
 }
 
-void set_cursor_position(unsigned short x, unsigned short y){
-	unsigned short position = scale_cursor_position(x, y);
+void set_cursor_position(uint_16 position){
+	if(position >= 2000) // Check if the wanted position is out of bounds
+		position = 1999; 
+
 	outb(0x3D4, 0x0F);
-	outb(0x3D5, (unsigned char)(position & 0xFF));
+	outb(0x3D5, (uint_8)(position & 0xFF));
 	outb(0x3D4, 0x0E);
-	outb(0x3D5, (unsigned char)((position >> 8) & 0xFF));
+	outb(0x3D5, (uint_8)((position >> 8) & 0xFF));
 
 	cursorPos = position;
 }
--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,6 +1,6 @@
 #include "PrintText.cpp"
 
 extern "C" void _start(){
-	set_cursor_position(79, 12);
+	set_cursor_position(scale_cursor_position(0, 0)); //Set the cursor to the top left by default
 	return;
 }
