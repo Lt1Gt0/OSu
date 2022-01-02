fb8a977b413f0790bf9c94c880954f75315c0aff
BuildTools <simonkaminski1100@gmail.com>
2022-01-01 20:14:59 -0700

    Updated bootloader

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -1,9 +1,109 @@
 #include <efi.h>
 #include <efilib.h>
+#include <elf.h>
+
+typedef unsigned long long size_t ;
+
+EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable){
+	EFI_FILE* LoadedFile;
+
+	EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
+	SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
+
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* FileSystem;
+	SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&FileSystem); //Get file system where booted from
+
+	//If the directory is null set the directory to the root of the file system
+	if(Directory == NULL){
+		FileSystem->OpenVolume(FileSystem, &Directory);
+	}
+
+	EFI_STATUS s = Directory->Open(Directory, &LoadedFile, Path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
+	if (s != EFI_SUCCESS) return NULL;
+	return LoadedFile;
+}
+
+// Memory Compare function
+int memcmp(const void* aptr, const void* bptr, size_t n){
+	const unsigned char* a = aptr, *b = bptr;
+	for(size_t i = 0; i < n; i++){
+		if(a[i] < b[i]) return -1;
+		else if(a[i] > b[i]) return 1;
+	}
+	return 0;
+}
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-	InitializeLib(ImageHandle, SystemTable);
+	InitializeLib(ImageHandle, SystemTable); //Set up the UEFI environment commands
 	Print(L"Bootloader Initialized\n\r");
+
+	EFI_FILE* Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable); // Load file 
+	if (LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable) == NULL){
+		Print(L"Could Not Load Kernel \n\r");
+	} else {
+		Print(L"Kernel Loaded Successfully\n\r");
+	}
+
+	Elf64_Ehdr header; // Create ELF Header
+	{
+		UINTN FileInfoSize;
+		EFI_FILE_INFO* FileInfo;
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, NULL); //Set the file info size to the size of the kernel
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, FileInfoSize, (void**)&FileInfo); // Allocate memory for the ELF Header
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, (void**)&FileInfo);
+
+		UINTN size = sizeof(header);
+		Kernel->Read(Kernel, &size, &header); // Read amount of bytes in size from the header
+	}
+
+	// Check if kernel header is correct
+	if(
+		memcmp(&header.e_ident[EI_MAG0], ELFMAG, SELFMAG) != 0 ||
+		header.e_ident[EI_CLASS] != ELFCLASS64 ||
+		header.e_ident[EI_DATA] != ELFDATA2LSB ||
+		header.e_type != ET_EXEC ||
+		header.e_machine != EM_X86_64 ||
+		header.e_version != EV_CURRENT
+	)
+	{
+		Print(L"Kernel format is bad\n\r");
+	} else {
+		Print(L"Kernel header successfully verified\n\r");
+	}
+
+	Elf64_Phdr* phdrs;
+	{
+		Kernel->SetPosition(Kernel, header.e_phoff); // Set offset in bytes when read
+		UINTN size = header.e_phnum * header.e_phentsize; // Program header num * Program header entry size
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, size, (void**)&phdrs);
+		Kernel->Read(Kernel, &size, phdrs);
+	}
+
+	// Go through each program header and load their binary information
+	for(
+		Elf64_Phdr* phdr = phdrs;
+		(char*)phdr < (char*)phdrs + header.e_phnum * header.e_phentsize;
+		phdr = (Elf64_Phdr*)((char*)phdr + header.e_phentsize)
+	)
+	{
+		switch(phdr->p_type){
+			case PT_LOAD:
+			{
+				int pages = (phdr->p_memsz + 0x1000 - 1) / 0x1000; //Get memory size and round up
+				Elf64_Addr segment = phdr->p_paddr;
+				SystemTable->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, pages, &segment);
+				Kernel->SetPosition(Kernel, phdr->p_offset);
+				UINTN size = phdr->p_filesz;
+				Kernel->Read(Kernel, &size, (void*)segment);
+				break;
+			}
+		}
+	}
+	Print(L"Kernel Loaded\n\r");
+
+	int(*KernelStart)() = ((__attribute__((sysv_abi)) int (*)() ) header.e_entry); // Define an integer function pointer at the address of header.e_entry with the attribute provided
+
+	Print(L"%d\r\n", KernelStart());
 
 	return EFI_SUCCESS; // Exit the UEFI application
 }
