471d62954bc65f2aae9cdd5429a01e9c34dcf348
BuildTools <simonkaminski1100@gmail.com>
2022-01-01 18:23:00 -0700

    Added gnu-efi/kernel/OVMFbin

---
+++ gnu-efi/bootloader/main.c
@@ -0,0 +1,9 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+	InitializeLib(ImageHandle, SystemTable);
+	Print(L"Bootloader Initialized\n\r");
+
+	return EFI_SUCCESS; // Exit the UEFI application
+}
---
+++ kernel/src/Kernel.cpp
@@ -0,0 +1,3 @@
+extern "C" int _start(){
+    return 123;
+}
