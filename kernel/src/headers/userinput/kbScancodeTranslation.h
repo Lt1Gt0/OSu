1468dd6de930715c9d654caf36ee8cccabb99b82
Zman2024 <minertlink5@gmail.com>
2022-04-12 20:34:39 -0700

    Add keyboard arrow keys support for scancode set 1

--- kernel/src/headers/userinput/kbScancodeTranslation.h
+++ kernel/src/headers/userinput/kbScancodeTranslation.h
@@ -8,6 +8,12 @@ namespace QWERTYKeyboard
     #define LeftShift   0x2A
     #define RightShift  0x36
     #define SpaceBar    0x39  
+	#define Special		0xE0
+
+	#define CursorUp	0x48
+	#define CursorRight	0x4D
+	#define CursorLeft	0x4B
+	#define CursorDown	0x50
 
     extern const char ASCIITable[];
     char Translate(uint8_t scancode, bool uppercase);
